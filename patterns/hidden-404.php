25ed0039089a1770984c63b3b1eaa21581604fc9
leph83 <info@phuc.at>
2023-05-10 15:43:28 +0200

    reduce theme.json and fixed 404

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Title: Hidden 404
  * Slug: pse-vorlage/hidden-404
@@ -7,48 +6,28 @@
  */
 ?>
 
+
+
 <!-- wp:group {
-    "tagName":"main",
-    "className":"main",
-    "style":{
-        "spacing":{
-            "margin":{
-                "top":"0"
-            }
-        }
-    },
-    "layout":{
-        "type":"constrained"
-    }
+	"layout":{
+		"type":"constrained"
+	}
 } -->
-<main class="wp-block-group main" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
+<div class="wp-block-group">
 
 	<!-- wp:heading {"level":1} -->
-	<h1 >
+	<h1>
 		<?php echo esc_html_x('404', 'Error code for a webpage that is not found.', 'pse-vorlage'); ?>
 	</h1>
 	<!-- /wp:heading -->
 
+	<!-- wp:paragraph -->
+	<p>
+	<?php echo esc_html_x('This page could not be found.', 'Message to convey that a webpage could not be found', 'pse-vorlage'); ?>
+	</p>
+	<!-- /wp:paragraph -->
 
-
-	<!-- wp:group {
-		"style":{
-			"spacing":{
-				"margin":{
-					"top":"0",
-					"bottom":"0"
-				}
-			}
-		},
-		"layout":{
-			"type":"default"
-		}
-	} -->
-	<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:paragraph -->
-		<?php echo esc_html_x( 'This page could not be found.', 'Message to convey that a webpage could not be found', 'pse-vorlage' ); ?>
-		<!-- /wp:paragraph -->
-
-		<!-- wp:search {
+	<!-- wp:search {
 			"label":"Search",
 			"showLabel":false,
 			"placeholder":"Search...",
@@ -58,10 +37,5 @@
 			"buttonUseIcon":true,
 			"align":"center"
 		} /-->
-	</div>
-	<!-- /wp:group -->
-
-
-
-</main>
+</div>
 <!-- /wp:group -->
