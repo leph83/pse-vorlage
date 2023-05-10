79ab4a959e84c23aba21b8cca817b997df4640ef
leph83 <info@phuc.at>
2023-05-10 20:12:20 +0200

    design the search the way I like it

--- patterns/hidden-no-results.php
+++ patterns/hidden-no-results.php
@@ -5,10 +5,10 @@
  * Inserter: no
  */
 ?>
+
+
 <!-- wp:paragraph -->
 <p>
-<?php echo esc_html_x( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'pse-vorlage' ); ?>
+    <?php echo esc_html_x('Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'Message explaining that there are no results returned from a search', 'pse-vorlage'); ?>
 </p>
 <!-- /wp:paragraph -->
-
-<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'pse-vorlage' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'pse-vorlage' ); ?>","showLabel":false,"buttonText":"<?php esc_attr_e( 'Search', 'pse-vorlage' ); ?>","buttonUseIcon":true} /-->
