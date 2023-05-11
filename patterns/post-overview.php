0ce42bd1864841e607d8475e42bbfb91c02e419c
leph83 <info@phuc.at>
2023-05-11 12:29:08 +0200

    reduced files

---
+++ patterns/post-overview.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Post
+ * Slug: post-overview
+ * Categories: query
+ * Keywords: post
+ * Block Types: core/template-part/post-overview
+ */
+?>
+
+<!-- wp:post-featured-image {
+    "isLink":true,
+    "width":"100%",
+    "height":"max(15vw, 30vh)"
+} /-->
+
+<!-- wp:post-title {
+    "isLink":true
+} /-->
+
+<!-- wp:post-excerpt /-->
+
+<!-- wp:post-date {
+    "isLink":true
+} /-->
+
+<!-- wp:spacer {"height":"var(--wp--preset--spacing--50)"} -->
+<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
